6318e34b2ff1cef0fd9df86d75738b959409bf18
lucky9457 <luckyreddychintu@gmail.com>
2024-02-28 15:18:01 +0000

    main

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,67 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+const bslogsData = [
+  {
+    id: 1,
+    title: 'Blog 1',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-1-img.png',
+    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+    author: 'Author Name',
+    topic: 'React.js',
+  },
+  {
+    id: 2,
+    title: 'Blog 2',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-2-img.png',
+    avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
+    author: 'Author Name',
+    topic: 'React.js',
+  },
+]
+
+class BlogsList extends Component {
+  state = {blogsData: [], isloading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    console.log(data)
+    const dataresponse = data.map(each => ({
+      id: each.id,
+      title: each.title,
+      topic: each.topic,
+      imageUrl: each.image_url,
+      avatarUrl: each.avatar_url,
+      author: each.author,
+    }))
+    console.log(dataresponse)
+    this.setState({
+      blogsData: dataresponse,
+      isloading: false,
+    })
+  }
+
+  render() {
+    const {blogsData, isloading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isloading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
